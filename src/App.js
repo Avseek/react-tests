be179af3c9c1b222a01f4b0f3edb09f11de20d29
Avsek <juffin8@gmail.com>
2024-11-04 11:57:45 +0300

    no ceil

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div class="container">
+        <div class="label">Label 1 (number):&nbsp;</div>
+        <div class="break"></div>
+        <div class="inputContainer"><input type="text" inputmode="numeric"></input></div>
+        <div class="break"></div>
+
+        <div class="label">Label 2:&nbsp;</div>
+        <div class="break"></div>
+        <div class="inputContainer"><input type="text"></input></div>
+        <div class="break"></div>
+
+        <div class="label">Label 3:&nbsp;</div>
+        <div class="break"></div>
+        <div class="inputContainer"><input type="text"></input></div>
+        <div class="break"></div>
+        
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+        <div class="label">Label 4 (number):&nbsp;</div>
+        <div class="break"></div>
+        <div class="inputContainer"><input type="number"  inputmode="numeric"></input></div>
+        <div class="break"></div>
+
+        <div class="label">Label 5:&nbsp;</div>
+        <div class="break"></div>
+        <div class="inputContainer"><input type="text"></input></div>
+      </div>
+    </>
+  )
 }
 
 export default App;
